2e9aff2a5852e2aad990a2484866f5a60f799910
nevds1d <scottsdale787@gmail.com>
2022-04-07 21:26:02 +0500

    add files, script

---
+++ src/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrowLeft.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrowRight.svg"></button>',
+		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					adaptiveHeight:true,
+					slidesToShow: 1,
+					slidesToScroll: 1,
+					dots: true,
+					arrows: false
+				}
+			}	
+		]
+      });
+  });
